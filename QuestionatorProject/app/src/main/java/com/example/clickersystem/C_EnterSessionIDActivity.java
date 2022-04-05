b1530439f1975e399957dfc0a236b9ad55441c3b
Kenny <tkm123123123@hotmail.com>
2022-04-05 22:18:39 +0800

    Upgraded UI appearance

--- QuestionatorProject/app/src/main/java/com/example/clickersystem/C_EnterSessionIDActivity.java
+++ QuestionatorProject/app/src/main/java/com/example/clickersystem/C_EnterSessionIDActivity.java
@@ -27,13 +27,11 @@ import java.net.URL;
 
 public class C_EnterSessionIDActivity extends AppCompatActivity
 {
-    private Button enterquiz;
     private EditText sessionid;
     URL target;
     HttpURLConnection conn;
     private static final String TAG = C_EnterSessionIDActivity.class.getSimpleName();
 
-    private EditText id1, id2, id3, id4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -47,12 +45,7 @@ public class C_EnterSessionIDActivity extends AppCompatActivity
 
 //        Toast.makeText(this, "Send", Toast.LENGTH_LONG).show();
 
-        enterquiz = (Button) findViewById(R.id.btnEnterQuiz);
         sessionid = (EditText) findViewById(R.id.txtEnterSessionID);
-        id1 = (EditText) findViewById(R.id.txtID1);
-        id2 = (EditText) findViewById(R.id.txtID2);
-        id3 = (EditText) findViewById(R.id.txtID3);
-        id4 = (EditText) findViewById(R.id.txtID4);
 
         //auto detect 4-digit session ID input function//
         sessionid.addTextChangedListener(new TextWatcher() {
@@ -78,72 +71,7 @@ public class C_EnterSessionIDActivity extends AppCompatActivity
             }
         });
         //auto detect 4-digit session ID input function//
-
-        //4-digit code function//
-        id1.addTextChangedListener(idinputWatcher);
-        id2.addTextChangedListener(idinputWatcher);
-        id3.addTextChangedListener(idinputWatcher);
-        id4.addTextChangedListener(idinputWatcher);
-        //4-digit code function//
-
-        //Button function//
-        enterquiz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(C_EnterSessionIDActivity.this, D_WaitingActivity.class);
-                //String sessionidInput = sessionid.getText().toString();
-                String id1input = id1.getText().toString().trim();
-                String id2input = id2.getText().toString().trim();
-                String id3input = id3.getText().toString().trim();
-                String id4input = id4.getText().toString().trim();
-
-                if (id1input.equals("1") && id2input.equals("2") && id3input.equals("3") && id4input.equals("4"))
-                {
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
-                    C_EnterSessionIDActivity.this.finish();
-                }
-                else
-                {
-                    Toast.makeText(C_EnterSessionIDActivity.this, "Invalid Session ID, Please try again.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        //Button function//
     }
-
-    //4-digit code function//
-    private TextWatcher idinputWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        }
-
-        @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            String id1input = id1.getText().toString().trim();
-            String id2input = id2.getText().toString().trim();
-            String id3input = id3.getText().toString().trim();
-            String id4input = id4.getText().toString().trim();
-
-            if(id1input.length() == 1 && id2input.length() == 0 && id3input.length() == 0 && id4input.length() == 0)
-            {
-                id2.requestFocus();
-            }
-            if(id1input.length() == 1 && id2input.length() == 1 && id3input.length() == 0 && id4input.length() == 0)
-            {
-                id3.requestFocus();
-            }
-            if(id1input.length() == 1 && id2input.length() == 1 && id3input.length() == 1 && id4input.length() == 0)
-            {
-                id4.requestFocus();
-            }
-        }
-
-        @Override
-        public void afterTextChanged(Editable editable) {
-        }
-    };
-    //4-digit code function//
 
     // Run the HTTP request in a background thread, separating from the main UI thread
     private class HttpTask extends AsyncTask<String, Void, String> {
--- QuestionatorProject/app/src/main/java/com/example/clickersystem/E_AnswerOptionActivity.java
+++ QuestionatorProject/app/src/main/java/com/example/clickersystem/E_AnswerOptionActivity.java
@@ -1,17 +1,31 @@
 package com.example.clickersystem;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.RectShape;
+import android.graphics.drawable.shapes.RoundRectShape;
+import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.shape.ShapePath;
+
 public class E_AnswerOptionActivity extends AppCompatActivity
 {
     private TextView qnTimer;
+    private Button optionA;
+    private Button optionB;
+    private Button optionC;
+    private Button optionD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,8 +34,92 @@ public class E_AnswerOptionActivity extends AppCompatActivity
         setContentView(R.layout.e_student_ans_option_page);
 
         qnTimer = (TextView) findViewById(R.id.countdownTimer);
+        optionA = (Button) findViewById(R.id.btnA);
+        optionB = (Button) findViewById(R.id.btnB);
+        optionC = (Button) findViewById(R.id.btnC);
+        optionD = (Button) findViewById(R.id.btnD);
 
-        new CountDownTimer(50000, 1000)
+        optionA.setBackground(getResources().getDrawable(R.drawable.button_a_design));
+        optionB.setBackground(getResources().getDrawable(R.drawable.button_b_design));
+        optionC.setBackground(getResources().getDrawable(R.drawable.button_c_design));
+        optionD.setBackground(getResources().getDrawable(R.drawable.button_d_design));
+
+        optionA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                optionA.setBackground(getResources().getDrawable(R.color.black));
+//                optionA.setTextColor(getResources().getColor(R.color.white));
+                /*ShapeDrawable borderA = new ShapeDrawable();
+                borderA.setShape(new RectShape());
+                borderA.getPaint().setColor(Color.RED);
+                borderA.getPaint().setStrokeWidth(30f);
+                borderA.getPaint().setStyle(Paint.Style.STROKE);
+                optionA.setBackground(borderA);*/
+                optionA.setBackground(getResources().getDrawable(R.drawable.button_a_design_pressed));
+                optionB.setBackground(getResources().getDrawable(R.drawable.button_b_design));
+                optionC.setBackground(getResources().getDrawable(R.drawable.button_c_design));
+                optionD.setBackground(getResources().getDrawable(R.drawable.button_d_design));
+            }
+        });
+
+        optionB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                optionB.setBackground(getResources().getDrawable(R.color.black));
+//                optionB.setTextColor(getResources().getColor(R.color.white));
+                /*ShapeDrawable borderB = new ShapeDrawable();
+                borderB.setShape(new RectShape());
+                borderB.getPaint().setColor(Color.BLUE);
+                borderB.getPaint().setStrokeWidth(30f);
+                borderB.getPaint().setStyle(Paint.Style.STROKE);
+                optionB.setBackground(borderB);*/
+
+                optionB.setBackground(getResources().getDrawable(R.drawable.button_b_design_pressed));
+                optionA.setBackground(getResources().getDrawable(R.drawable.button_a_design));
+                optionC.setBackground(getResources().getDrawable(R.drawable.button_c_design));
+                optionD.setBackground(getResources().getDrawable(R.drawable.button_d_design));
+            }
+        });
+
+        optionC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                optionC.setBackground(getResources().getDrawable(R.color.black));
+//                optionC.setTextColor(getResources().getColor(R.color.white));
+                /*ShapeDrawable borderC = new ShapeDrawable();
+                borderC.setShape(new RectShape());
+                borderC.getPaint().setColor(Color.GREEN);
+                borderC.getPaint().setStrokeWidth(30f);
+                borderC.getPaint().setStyle(Paint.Style.STROKE);
+                optionC.setBackground(borderC);*/
+
+                optionC.setBackground(getResources().getDrawable(R.drawable.button_c_design_pressed));
+                optionA.setBackground(getResources().getDrawable(R.drawable.button_a_design));
+                optionB.setBackground(getResources().getDrawable(R.drawable.button_b_design));
+                optionD.setBackground(getResources().getDrawable(R.drawable.button_d_design));
+            }
+        });
+
+        optionD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                optionD.setBackground(getResources().getDrawable(R.color.black));
+//                optionD.setTextColor(getResources().getColor(R.color.white));
+                /*ShapeDrawable borderD = new ShapeDrawable();
+                borderD.setShape(new RectShape());
+                borderD.getPaint().setColor(Color.YELLOW);
+                borderD.getPaint().setStrokeWidth(30f);
+                borderD.getPaint().setStyle(Paint.Style.STROKE);
+                optionD.setBackground(borderD);*/
+
+                optionD.setBackground(getResources().getDrawable(R.drawable.button_d_design_pressed));
+                optionA.setBackground(getResources().getDrawable(R.drawable.button_a_design));
+                optionB.setBackground(getResources().getDrawable(R.drawable.button_b_design));
+                optionC.setBackground(getResources().getDrawable(R.drawable.button_c_design));
+            }
+        });
+
+        new CountDownTimer(5000, 1000)
         {
             public void onTick(long millisRemaining)
             {
