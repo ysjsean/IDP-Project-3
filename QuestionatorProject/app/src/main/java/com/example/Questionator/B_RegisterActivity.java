6c22990887c707088b1d14dd053dc3d87be3a5a5
Kenny <tkm123123123@hotmail.com>
2022-04-13 20:16:38 +0800

    Final version for android app

--- QuestionatorProject/app/src/main/java/com/example/Questionator/B_RegisterActivity.java
+++ QuestionatorProject/app/src/main/java/com/example/Questionator/B_RegisterActivity.java
@@ -1,17 +1,39 @@
 package com.example.Questionator;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Layout;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
 
-public class B_RegisterActivity extends AppCompatActivity
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class B_RegisterActivity extends AppCompatActivity implements AsyncResponse
 {
     private Button registeracc;
     private Button backtologin;
@@ -19,10 +41,22 @@ public class B_RegisterActivity extends AppCompatActivity
     private String emailtext;
     private String emailPattern;
     private ScrollView regBG;
+    private TextInputLayout namelayout;
+    private TextInputLayout emaillayout;
+    private TextInputLayout contactlayout;
+    private TextInputLayout pwlayout;
+    private TextInputLayout cpwlayout;
+
+    private static final String TAG = B_RegisterActivity.class.getSimpleName();
+    URL target;
+    HttpURLConnection conn;
+
+    HttpTask httpTask = new HttpTask();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.b_student_registration_page);
 
@@ -34,11 +68,93 @@ public class B_RegisterActivity extends AppCompatActivity
         pw = (EditText) findViewById(R.id.passwordInput);
         cpw = (EditText) findViewById(R.id.confirmpasswordInput);
         regBG = (ScrollView) findViewById(R.id.regPage);
+        namelayout = (TextInputLayout) findViewById(R.id.nameLayout);
+        emaillayout = (TextInputLayout) findViewById(R.id.emailLayout);
+        contactlayout = (TextInputLayout) findViewById(R.id.contactLayout);
+        pwlayout = (TextInputLayout) findViewById(R.id.passwordLayout);
+        cpwlayout = (TextInputLayout) findViewById(R.id.confirmpasswordLayout);
 
         AnimationDrawable animationDrawable = (AnimationDrawable) regBG.getBackground();
         animationDrawable.setEnterFadeDuration(3000);
         animationDrawable.setExitFadeDuration(5000);
         animationDrawable.start();
+
+        httpTask.delegate = this;
+
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                namelayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                emaillayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        contact.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                contactlayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        pw.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                pwlayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        cpw.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                cpwlayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
 
         registeracc.setOnClickListener(new View.OnClickListener()
         {
@@ -48,33 +164,64 @@ public class B_RegisterActivity extends AppCompatActivity
                 Intent intent = new Intent(B_RegisterActivity.this, A_LoginPageActivity.class);
                 emailtext = email.getText().toString().trim();
                 emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-                String pwinput = pw.getText().toString();
-                String cpwinput = cpw.getText().toString();
+                String pwinput = pw.getText().toString().trim();
+                String cpwinput = cpw.getText().toString().trim();
+                String studentname = name.getText().toString().trim();
+                String studentemail = email.getText().toString().trim();
+                String studentcontact = contact.getText().toString().trim();
+                String studentpw = pw.getText().toString().trim();
 
-                if(name.length() > 0 && email.length() > 0 && contact.length() > 0 && pw.length() > 0 && cpw.length() > 0)
+                if(name.length() == 0)
                 {
-                    if (!pwinput.equals(cpwinput) && !emailtext.matches(emailPattern))
-                    {
-                        Toast.makeText(B_RegisterActivity.this, "Invalid email", Toast.LENGTH_SHORT).show();
-                        Toast.makeText(B_RegisterActivity.this, "Passwords does not match!", Toast.LENGTH_SHORT).show();
-                    }
-                    else if (!emailtext.matches(emailPattern))
-                    {
-                        Toast.makeText(B_RegisterActivity.this, "Invalid email", Toast.LENGTH_SHORT).show();
-                    }
-                    else if (!pwinput.equals(cpwinput))
-                    {
-                        Toast.makeText(B_RegisterActivity.this, "Passwords does not match!", Toast.LENGTH_SHORT).show();
-                    }
-                    else if (pwinput.equals(cpwinput) && emailtext.matches(emailPattern))
-                    {
-                        Toast.makeText(B_RegisterActivity.this, "Registration Success!", Toast.LENGTH_SHORT).show();
-                        startActivity(intent);
-                    }
+                    namelayout.setError("Please enter your name");
                 }
-                else
+                else if(name.length() > 0 && name.length() < 6)
                 {
-                    Toast.makeText(B_RegisterActivity.this, "Please do not leave any fields empty", Toast.LENGTH_SHORT).show();
+                    namelayout.setError("Name should contain at least 6 characters");
+                }
+
+                if(email.length() == 0)
+                {
+                    emaillayout.setError("Please enter your email");
+                }
+                else if (email.length() > 0 && !emailtext.matches(emailPattern))
+                {
+                    emaillayout.setError("Invalid email format");
+                }
+
+                if(contact.length() == 0)
+                {
+                    contactlayout.setError("Please enter your contact number");
+                }
+                else if(contact.length() > 0 && contact.length() != 8)
+                {
+                    contactlayout.setError("Contact number should contain 8 digits");
+                }
+                else if((contact.getText().charAt(0) != '8' && contact.getText().charAt(0) != '9'))
+                {
+                    contactlayout.setError("Contact number should begin with '8' or '9'");
+                }
+
+                if(pw.length() == 0)
+                {
+                    pwlayout.setError("Please enter your password");
+                }
+                if(cpw.length() == 0)
+                {
+                    cpwlayout.setError("Please confirm your password");
+                }
+                else if(pw.length() > 0 && pw.length() < 6)
+                {
+                    pwlayout.setError("Password should contain at least 6 characters");
+                }
+                else if(pwinput.length() >= 6 && !pwinput.equals(cpwinput))
+                {
+                    cpwlayout.setError("Password does not match!");
+                }
+                else if(name.length() >= 6 && emailtext.matches(emailPattern) && contact.length() == 8) //When registration is successful
+                {
+                    String url = String.format("%s/api/users/student/register/%s/%s/%s/%s", getResources().getString(R.string.api_url),studentname,studentemail,studentcontact,studentpw);
+                    httpTask.execute(url);
                 }
             }
         });
@@ -88,5 +235,78 @@ public class B_RegisterActivity extends AppCompatActivity
                 startActivity(intent);
             }
         });
+    }
+
+    private void onUpdate(){
+        httpTask = new HttpTask();
+        httpTask.delegate = this;
+    }
+
+    private void success(){
+//        Snackbar.make(registeracc, "Registration Successful!", Snackbar.LENGTH_LONG).show();
+        Toast.makeText(B_RegisterActivity.this, "Registration Successful!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(B_RegisterActivity.this, A_LoginPageActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        B_RegisterActivity.this.finish();
+    }
+
+    @Override
+    public void processFinish(String result) {
+        JSONObject json;
+        JSONArray jsonArray;
+        try{
+            json = new JSONObject(result);
+            if(json.getBoolean("success")){
+                success();
+            } else {
+                Toast.makeText(B_RegisterActivity.this, "Try again", Toast.LENGTH_LONG).show();
+            }
+//                Log.d("Data: ", "doInBackground: " + json.getJSONObject(i).getString("answer"));
+//                Toast.makeText(A_LoginPageActivity.this, "" + jsonArr.length(), Toast.LENGTH_SHORT).show();
+
+        } catch (JSONException e){
+            Log.d("Error: ", "err: " + e.toString());
+        }
+        httpTask.cancel(true);
+        onUpdate();
+    }
+
+    private class HttpTask extends AsyncTask<String, Void, String> {
+        public AsyncResponse delegate = null;
+        @Override
+        protected String doInBackground(String... strURLs) {
+            URL url = null;
+            HttpURLConnection conn = null;
+            try {
+                url = new URL(strURLs[0]);
+                conn = (HttpURLConnection) url.openConnection();
+                // Get the HTTP response code (e.g., 200 for "OK", 404 for "Not found")
+                // and pass a string description in result to onPostExecute(String result)
+                int responseCode = conn.getResponseCode();
+                Log.d("Code: ", " " + responseCode);
+                if (responseCode == HttpURLConnection.HTTP_OK) {  // 200
+                    InputStream response = conn.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(response));
+                    String output = br.readLine();
+                    Log.d("Results: ", "output: " + output);
+                    return output;
+//                    return "OK (" + responseCode + ")";
+                } else {
+                    return "[{Fail: " + responseCode + "}]";
+                }
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        // Displays the result of the AsyncTask.
+        // The String result is passed from doInBackground().
+        @Override
+        protected void onPostExecute(String result) {
+//            txtResponse.setText(result);  // put it on TextView
+//            JSONArray jsonArr;
+            delegate.processFinish(result);
+        }
     }
 }
--- QuestionatorProject/app/src/main/java/com/example/Questionator/F_FinalPageActivity.java
+++ QuestionatorProject/app/src/main/java/com/example/Questionator/F_FinalPageActivity.java
@@ -3,24 +3,65 @@ package com.example.Questionator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class F_FinalPageActivity extends AppCompatActivity
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class F_FinalPageActivity extends AppCompatActivity implements AsyncResponse
 {
     private Button takeanotherquiz;
     private Button logout;
+    private TextView totalscore;
+    private TextView lblresults;
     private static final String TAG = F_FinalPageActivity.class.getSimpleName();
+    URL target;
+    HttpURLConnection conn;
+    HttpTask httpTask = new HttpTask();
+    long score;
+    int finalscore;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.f_student_finalscore_page);
+        final SharedPreferences profilePrefs = getSharedPreferences("profile", MODE_PRIVATE);
+        final SharedPreferences currentPrefs = getSharedPreferences("current", MODE_PRIVATE);
 
+        totalscore = (TextView) findViewById(R.id.finalscore);
+        lblresults = (TextView) findViewById(R.id.labelResultsMsg);
         takeanotherquiz = (Button) findViewById(R.id.btnTakeAnotherQuiz);
         logout = (Button) findViewById(R.id.btnLogOut);
+        httpTask.delegate = this;
+
+        score = currentPrefs.getLong("totalScore", 0);
+        finalscore = (int) ((score/5.0) * 100);
+        totalscore.setText(String.valueOf(finalscore) + "%");
+
+        if (finalscore >= 50)
+        {
+            lblresults.setText("Congratulations! You have passed.");
+        }
+        else
+        {
+            lblresults.setText("Try harder next time!");
+        }
 
         takeanotherquiz.setOnClickListener(new View.OnClickListener()
         {
@@ -39,11 +80,96 @@ public class F_FinalPageActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
+                SharedPreferences.Editor editor = profilePrefs.edit();
+                editor.putString("userid:","");
+                editor.putString("roleid:","");
+                editor.putString("name", "");
+                editor.putString("password", "");
+                editor.putString("email", "");
+                editor.commit();
+
                 Intent intent = new Intent(F_FinalPageActivity.this, A_LoginPageActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
-                finish();
+                F_FinalPageActivity.this.finish();
             }
         });
+    }
+
+    private void onUpdate(){
+        httpTask = new HttpTask();
+        httpTask.delegate = this;
+    }
+
+    private void success(String username)
+    {
+        Toast.makeText(F_FinalPageActivity.this, "Entry Success", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(F_FinalPageActivity.this, A_LoginPageActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        F_FinalPageActivity.this.finish();
+    }
+
+    @Override
+    public void processFinish(String result)
+    {
+        JSONObject json;
+        JSONArray jsonArray;
+        try{
+            json = new JSONObject(result);
+            if(json.getBoolean("success"))
+            {
+                jsonArray = json.getJSONArray("data");
+                if(jsonArray.length() > 0)
+                {
+                    success(jsonArray.getJSONObject(0).getString("username"));
+                }
+            }
+        }
+        catch (JSONException e)
+        {
+            Log.d("Error: ", "err: " + e.toString());
+        }
+        httpTask.cancel(true);
+        onUpdate();
+    }
+
+    // Run the HTTP request in a background thread, separating from the main UI thread
+    private class HttpTask extends AsyncTask<String, Void, String> {
+        public AsyncResponse delegate = null;
+        @Override
+        protected String doInBackground(String... strURLs) {
+            URL url = null;
+            HttpURLConnection conn = null;
+            try {
+                url = new URL(strURLs[0]);
+                conn = (HttpURLConnection) url.openConnection();
+                // Get the HTTP response code (e.g., 200 for "OK", 404 for "Not found")
+                // and pass a string description in result to onPostExecute(String result)
+                int responseCode = conn.getResponseCode();
+                Log.d("Code: ", " " + responseCode);
+                if (responseCode == HttpURLConnection.HTTP_OK) {  // 200
+                    InputStream response = conn.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(response));
+                    String output = br.readLine();
+                    Log.d("Results: ", "output: " + output);
+                    return output;
+//                    return "OK (" + responseCode + ")";
+                } else {
+                    return "[{Fail: " + responseCode + "}]";
+                }
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        // Displays the result of the AsyncTask.
+        // The String result is passed from doInBackground().
+        @Override
+        protected void onPostExecute(String result) {
+//            txtResponse.setText(result);  // put it on TextView
+//            JSONArray jsonArr;
+            delegate.processFinish(result);
+        }
     }
 }
